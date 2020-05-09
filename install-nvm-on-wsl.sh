a29d0dba95f8dcf423ef37d104551970c4a64f48
Asapdotid <asapdotid@gmail.com>
2020-05-10 04:24:43 +0700

    fixing code install nvm

--- install-nvm-on-wsl.sh
+++ install-nvm-on-wsl.sh
@@ -4,7 +4,8 @@ nvm_version=v0.35.3
 node_version=12.16.3
 echo "Install NVM"
 curl -o nvm-install.sh https://raw.githubusercontent.com/nvm-sh/nvm/${nvm_version}/install.sh && sh nvm-install.sh;
-rm -rf nvm-install.shtee -a $HOME/.zshrc << END
+rm -rf nvm-install.sh
+tee -a $HOME/.zshrc << END
 # NVM
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
