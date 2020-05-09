3462b732b99a4fb3f82f0a66defd8d57ad9ee50f
Asapdotid <asapdotid@gmail.com>
2020-05-10 04:37:14 +0700

    fixing code EOF install nvm

--- install-nvm-on-wsl.sh
+++ install-nvm-on-wsl.sh
@@ -8,8 +8,8 @@ rm -rf nvm-install.sh
 tee -a $HOME/.zshrc << END
 # NVM
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "\$NVM_DIR/nvm.sh" ] && \. "\$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "\$NVM_DIR/bash_completion" ] && \. "\$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 END
 echo "Reload ZSHRC"
 source ~/.zshrc
