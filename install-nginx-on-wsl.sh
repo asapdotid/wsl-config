bf07a5d926158a73acc34a995ca635c83ebcad8e
Asapdotid <asapdotid@gmail.com>
2020-05-09 19:19:55 +0700

    update installation lemp for ubuntu 20.04

--- install-nginx-on-wsl.sh
+++ install-nginx-on-wsl.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sudo add-apt-repository -y ppa:nginx/stable
+echo "Install Nginx"
 sudo apt-get update
 sudo apt-get install nginx -y
+echo "Nginx version..."
+nginx -v
+echo "Done"
--- install-php-fpm-on-wsl.sh
+++ install-php-fpm-on-wsl.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 php_vesion=7.4
-sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update
-sudo apt-get install wget zip unzip -y
 echo "Install PHP ${php_vesion}"
 sudo apt-get install php${php_vesion}-fpm -y
 echo "PHP version..."
